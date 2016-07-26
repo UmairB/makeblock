8c3ee443f0fc3816a648adab85ad4aaf35a5cd58
Umair Butt <umair.butt@hotmail.co.uk>
2016-07-26 21:54:28 +0100

    Added the bot file

---
+++ server/bot.ts
@@ -0,0 +1,48 @@
+let MegaPi = require("megapi").MegaPi;
+
+export class Bot {
+    private bot: any;
+    private _motor: Motor;
+    private _ultrasonicSensor: UltrasonicSensor;
+
+    public get motor() : Motor {
+        return this._motor;
+    }
+
+    public get ultrasonicSensor() : UltrasonicSensor {
+        return this._ultrasonicSensor;
+    }
+
+    constructor(port: string, onStart: () => void) {
+        this.bot = new MegaPi(port, onStart);
+
+        this._motor = new Motor(this.bot);
+        this._ultrasonicSensor = new UltrasonicSensor(this.bot);
+    }
+}
+
+class UltrasonicSensor {
+    private bot: any;
+    constructor(bot: any) {
+        this.bot = bot;
+    }
+
+    read(port: number, onRead: (value: number) => void) {
+        this.bot.ultrasonicSensorRead(port, onRead);
+    }
+}
+
+class Motor {
+    private bot: any;
+    constructor(bot: any) {
+        this.bot = bot;
+    }
+
+    stop() {
+        this.bot.dcMotorStop();
+    }
+
+    run(port: number, speed: number) {
+        this.bot.dcMotorRun(port, speed);
+    }
+}
